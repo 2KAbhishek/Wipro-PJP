67ae0460c89c47a637791371c8ef3b6c2902b150
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:31 +0530

    Add ./Flow Control Statements/For2.java

---
+++ Topics/Flow Control Statements/For2.java
@@ -0,0 +1,9 @@
+public class For2{
+    public static void main(String[] atgs){
+        for (int i = 23; i <= 57; i++){
+            if (i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
