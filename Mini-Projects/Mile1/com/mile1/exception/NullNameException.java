d367e150d490acd34afaa1e74dab131b3a2fe934
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:14:46 +0530

    Add ./Mile1/com/mile1/exception/NullNameException.java

---
+++ Mini-Projects/Mile1/com/mile1/exception/NullNameException.java
@@ -0,0 +1,8 @@
+package com.mile1.exception;
+
+public class NullNameException extends Exception {
+    @Override
+    public String toString() {
+        return "NullNameException occurred";
+    }
+}
