87a8e202ebc8b236544b86865ab344e3e1eadf8b
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:14:46 +0530

    Add ./TM4/com/tm3/tests/UnitTest.java

---
+++ Mini-Projects/TM4/com/tm3/tests/UnitTest.java
@@ -0,0 +1,17 @@
+package com.tm3.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    FDAccountTest.class,
+    RDAccountTest.class,
+    SBAccountTest.class
+})
+
+public class UnitTest {
+
+
+
+}
