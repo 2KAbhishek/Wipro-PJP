e0c3dae379625e47bb1884275a46c5c76a357840
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:32 +0530

    Add ./Inheritance/Person.java

---
+++ Topics/Inheritance/Person.java
@@ -0,0 +1,15 @@
+public class Person{
+    String name;
+
+    Person(String name){
+        this.name = name;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    String getName(){
+        return this.name;
+    }
+}
