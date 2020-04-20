6ead89f3c62ca0746e7e049a24d3298bbd685e9c
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:14:46 +0530

    Add ./TM3/com/tm3/bean/SBAccount.java

---
+++ Mini-Projects/TM3/com/tm3/bean/SBAccount.java
@@ -0,0 +1,33 @@
+package com.tm3.bean;
+
+public class SBAccount extends Account {
+
+    @Override
+    public double calculateInterest() {
+        String accountType = "Normal";
+        if (accountType.equals("Normal")) setInterestRate(4);
+
+        double amount = getAmount();
+        double interestRate = getInterestRate();
+        double interest = amount * interestRate / 100.0;
+
+        return interest;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(int interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+}
