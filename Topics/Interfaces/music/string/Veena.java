4b4afb96cd4c3179b02e55f1bef57ebfa57060ac
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:31 +0530

    Add ./Interfaces/music/string/Veena.java

---
+++ Topics/Interfaces/music/string/Veena.java
@@ -0,0 +1,10 @@
+package music.string;
+
+import music.Playable;
+
+public class Veena implements Playable {
+    @Override
+    public void play(){
+        System.out.println("Playing Veena.");
+    }
+}
