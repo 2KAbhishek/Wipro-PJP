e1a46fed360be62ea1a897e037a082b2a8d189d2
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:32 +0530

    Add ./Packages/com/automobile/Vehicle.java

---
+++ Topics/Packages/com/automobile/Vehicle.java
@@ -0,0 +1,7 @@
+package com.automobile;
+
+public abstract class Vehicle {
+    public abstract String getModelName();
+    public abstract String getRegistrationNumber();
+    public abstract String getOwnerName();
+}
