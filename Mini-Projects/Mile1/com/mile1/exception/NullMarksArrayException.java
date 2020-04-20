08967edb70c36a2d5a0ce4ea3cbdd0ee5ff92d5a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:14:46 +0530

    Add ./Mile1/com/mile1/exception/NullMarksArrayException.java

---
+++ Mini-Projects/Mile1/com/mile1/exception/NullMarksArrayException.java
@@ -0,0 +1,8 @@
+package com.mile1.exception;
+
+public class NullMarksArrayException extends Exception {
+    @Override
+    public String toString() {
+        return "NullMarksArrayException occurred";
+    }
+}
