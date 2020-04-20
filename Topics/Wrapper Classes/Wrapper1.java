4f766643629a3d294e77314fd69ccedc7d72fb34
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:33 +0530

    Add ./Wrapper Classes/Wrapper1.java

---
+++ Topics/Wrapper Classes/Wrapper1.java
@@ -0,0 +1,29 @@
+public class Wrapper1 {
+
+    public static void main(String[] args) {
+        System.out.println("Integer range:");
+        System.out.println("min: " + Integer.MIN_VALUE);
+        System.out.println("max: " + Integer.MAX_VALUE);
+
+        System.out.println("Double range:");
+        System.out.println("min: " + Double.MIN_VALUE);
+        System.out.println("max: " + Double.MAX_VALUE);
+
+        System.out.println("Long range:");
+        System.out.println("min: " + Long.MIN_VALUE);
+        System.out.println("max: " + Long.MAX_VALUE);
+
+        System.out.println("Short range:");
+        System.out.println("min: " + Short.MIN_VALUE);
+        System.out.println("max: " + Short.MAX_VALUE);
+
+        System.out.println("Byte range:");
+        System.out.println("min: " + Byte.MIN_VALUE);
+        System.out.println("max: " + Byte.MAX_VALUE);
+
+        System.out.println("Float range:");
+        System.out.println("min: " + Float.MIN_VALUE);
+        System.out.println("max: " + Float.MAX_VALUE);
+    }
+
+}
