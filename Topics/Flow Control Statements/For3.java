25752cb5f3b344c0bef4c7012259154816ae97d0
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:31 +0530

    Add ./Flow Control Statements/For3.java

---
+++ Topics/Flow Control Statements/For3.java
@@ -0,0 +1,18 @@
+public class For3{
+    public static boolean isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < (int)Math.sqrt(n) + 1; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+    public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        String out = (isPrime(n)) ? "Prime" : "Not Prime";
+        System.out.println(out);
+    }
+}
