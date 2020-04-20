ed644d38d5b2a64522c4b51802f6143a81957162
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:31 +0530

    Add ./Language Basics/CLA3.java

---
+++ Topics/Language Basics/CLA3.java
@@ -0,0 +1,7 @@
+public class CLA3{
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("The sum of " + a + " and " + b + " is " + (a+b));
+    }
+}
