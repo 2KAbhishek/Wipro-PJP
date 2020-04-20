03a78ae999427eb8e30e137fbf69cf9bf9ad1ab5
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:32 +0530

    Add ./Packages/TestFoundation.java

---
+++ Topics/Packages/TestFoundation.java
@@ -0,0 +1,8 @@
+import test.Foundation;
+public class TestFoundation {
+    public static void main(String[] args) {
+        Foundation fn = new Foundation();
+        fn.var4 = 4;
+        System.out.println(fn.var4);
+    }
+}
