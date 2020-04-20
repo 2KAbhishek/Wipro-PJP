b487ec4edb6f46e9d633876741bb9c2561636cb5
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:14:46 +0530

    Add ./Mile1/com/mile1/exception/NullStudentException.java

---
+++ Mini-Projects/Mile1/com/mile1/exception/NullStudentException.java
@@ -0,0 +1,8 @@
+package com.mile1.exception;
+
+public class NullStudentException extends Exception {
+    @Override
+    public String toString() {
+        return "NullStudentException occurred";
+    }
+}
