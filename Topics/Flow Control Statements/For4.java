ee52383054c2474727d1db1f459f1ed0aa43470f
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:31 +0530

    Add ./Flow Control Statements/For4.java

---
+++ Topics/Flow Control Statements/For4.java
@@ -0,0 +1,20 @@
+public class For4{
+    public static boolean isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < (int)Math.sqrt(n) + 1; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+    public static void main(String[] args){
+        for (int n = 10; n <= 99; n++){
+            if(isPrime(n)){
+                System.out.println(n);
+            }
+        }
+    }
+}
