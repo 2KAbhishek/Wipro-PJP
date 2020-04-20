cb141e742b2022914a6e4c817a80bcc24da70154
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:32 +0530

    Add ./Test Suite/JunitTestSuite.java

---
+++ Topics/Test Suite/JunitTestSuite.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    UnitTest.class, UnitTest2.class, EmployeeTest.class
+})
+
+public class JunitTestSuite {
+
+}
