b586a5fe62ed740ae59908ba897234063287e350
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-20 14:13:31 +0530

    Add ./Flow Control Statements/For5.java

---
+++ Topics/Flow Control Statements/For5.java
@@ -0,0 +1,26 @@
+public class For5{
+    public static boolean isPrime(int n)
+    {
+        for (int i = 2; i < (int)Math.sqrt(n) + 1; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+    public static void main(String[] args){
+        if (args.length == 0){
+            System.out.println("Please enter an integer number");
+            System.exit(0);
+        }
+        int n = Integer.parseInt(args[0]);
+        if (n <= 1){
+            System.out.println(n + " is neither prime nor composite");
+        }
+        else if(isPrime(n)){
+            System.out.println(n + " is a prime number");
+        }
+        else{
+            System.out.println(n + " is not a prime number");
+        }
+    }
+}
