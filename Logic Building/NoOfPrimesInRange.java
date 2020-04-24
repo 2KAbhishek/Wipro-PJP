c31f3490903742f4143f030f93c4c0cd82e358e3
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-24 14:38:57 +0530

    Add NoOfPrimesInRange.java

---
+++ Logic Building/NoOfPrimesInRange.java
@@ -0,0 +1,16 @@
+public class NoOfPrimesInRange {
+    public static int noOfPrimesInRange(int input1, int input2) {
+        int primeCount = 0;
+
+        while(input1 <= input2) {
+            boolean isPrime = true;
+            for (int i = 2; i < input1; i++)
+                if (input1 % i == 0) isPrime = false;
+
+            if (isPrime) primeCount++;
+
+            input1++;
+        }
+        return primeCount;
+    }
+}
