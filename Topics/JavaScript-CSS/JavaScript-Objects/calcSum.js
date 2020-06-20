9dd570437e132c281c28fc2e3aa346d206a85b5d
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-20 08:40:22 +0530

    Add calcSum.js

---
+++ Topics/JavaScript-CSS/JavaScript-Objects/calcSum.js
@@ -0,0 +1,6 @@
+function sum() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    alert("Sum is : " + (num1 + num2));
+}
