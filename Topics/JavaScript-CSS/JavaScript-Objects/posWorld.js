67779a8ddc6fecf15dd3f3f488a47d4824de0946
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-20 08:40:22 +0530

    Add posWorld.js

---
+++ Topics/JavaScript-CSS/JavaScript-Objects/posWorld.js
@@ -0,0 +1,4 @@
+let txt = prompt("Enter text containing the word 'World' :");
+document.write("Entered text : " + txt + "<br>");
+let pos = txt.indexOf("World");
+document.write("Position of 'World' in text : " + pos);
