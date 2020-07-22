f1e2d9c16fc136997d8d1c7be73aaf9c63babdbe
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-23 02:36:01 +0530

    Add validateDetails

--- Mini-Projects/TM9/tm92/validate.js
+++ Mini-Projects/TM9/tm92/validate.js
@@ -7,3 +7,28 @@ function validatePass() {
         return false;
     }
 }
+
+function validateDetails() {
+    var userID = document.getElementById("userID").value;
+    var passwd = document.getElementById("passwd").value;
+    var dob = document.getElementById("dob").value;
+    var contact = document.getElementById("contact").value;
+
+    var userIDregex = /.{4,8}/;
+    var passwdRegex = /^(?=.*\d)(?=.*[A-Z]).{6,20}$/;
+    var contactRegex = /(?=.*\d){10}/;
+    var dobDate = new Date(dob);
+    var today = new Date();
+
+    if (
+        userID.match(userIDregex) &&
+        passwd.match(passwdRegex) &&
+        dobDate.getTime() < today.getTime() &&
+        contact.match(contactRegex)
+    ) {
+        return true;
+    } else {
+        alert("Invalid Details!");
+        return false;
+    }
+}
